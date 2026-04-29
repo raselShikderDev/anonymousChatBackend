d6e5b1fd60a4927497c38da21fe1a99e0a714847
Rasel-Shikder <rasel.sikder777.rk@gmail.com>
2026-04-30 04:52:17 +0600

    fixed databse for ssl and neon

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -14,52 +14,30 @@ export class DatabaseService implements OnModuleInit, OnModuleDestroy {
   constructor(private readonly configService: ConfigService) {}
 
   async onModuleInit() {
+    const databaseUrl = this.configService.databaseUrl;
+
+    const isCloud = databaseUrl.includes("sslmode");
+
     this.pool = new Pool({
-      connectionString: this.configService.databaseUrl,
+      connectionString: databaseUrl,
+      ssl: isCloud ? { rejectUnauthorized: false } : false,
     });
 
     this.db = drizzle(this.pool, { schema });
 
-    await migrate(this.db, {
-      // migrationsFolder: path.join(__dirname, '../../drizzle'),
-      migrationsFolder: path.join(process.cwd(), "drizzle"),
-    });
+    try {
+      await migrate(this.db, {
+        migrationsFolder: path.join(process.cwd(), "drizzle"),
+      });
 
-    console.log("✅ Database connected & migrated");
+      console.log(" Database connected & migrated");
+    } catch (err) {
+      console.error(" Migration failed:", err);
+      throw err;
+    }
   }
 
   async onModuleDestroy() {
     await this.pool.end();
   }
 }
-
-// import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-// import { drizzle } from 'drizzle-orm/node-postgres';
-// import { migrate } from 'drizzle-orm/node-postgres/migrator';
-// import { Pool } from 'pg';
-// import * as schema from './schema';
-// import { ConfigService } from '../config/config.service';
-// import * as path from 'path';
-
-// @Injectable()
-// export class DatabaseService implements OnModuleInit, OnModuleDestroy {
-//   public db: ReturnType<typeof drizzle<typeof schema>> |;
-//   private pool: Pool;
-
-//   constructor(private readonly configService: ConfigService) {}
-
-//   async onModuleInit() {
-//     this.pool = new Pool({
-//       connectionString: this.configService.databaseUrl,
-//     });
-//     this.db = drizzle(this.pool, { schema });
-
-//     await migrate(this.db, {
-//       migrationsFolder: path.join(__dirname, '../../drizzle'),
-//     });
-//   }
-
-//   async onModuleDestroy() {
-//     await this.pool.end();
-//   }
-// }
